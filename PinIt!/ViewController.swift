ccae2d5b1094794fc9b562b440807e7adfe0ada2
Robo Atenaga <roboatenaga@gmail.com>
2017-07-06 09:58:35 +0700

    Initial Commit

---
+++ PinIt!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinIt!
+//
+//  Created by Robo Atenaga on 7/6/17.
+//  Copyright © 2017 Robo Atenaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PinIt!Tests/PinIt_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PinIt_Tests.swift
+//  PinIt!Tests
+//
+//  Created by Robo Atenaga on 7/6/17.
+//  Copyright © 2017 Robo Atenaga. All rights reserved.
+//
+
+import XCTest
+@testable import PinIt_
+
+class PinIt_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
